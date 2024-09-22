3d949f15f5219baad7ab3ae9364d8bebb19d44c4
Felipe Bento <felipe.bento8@etec.sp.gov.br>
2024-09-22 12:33:21 -0300

    exercicios de switch case

---
+++ controle-de-fluxo/src/PlanoOperador.java
@@ -0,0 +1,23 @@
+public class PlanoOperador {
+    
+    public static void main(String[] args) {
+        
+        String plano = "M"; // M / T
+
+		switch (plano) {
+			case "T": {
+				System.out.println("5Gb Youtube");
+			}
+			case "M": {
+				System.out.println("Whats e Intagram grátis");
+			}
+			case "B": {
+				System.out.println("100 minutos de ligação");
+			}
+            /*Se optarem por usar switch / case, estudem um pouco mais sobre os conceitos de continue,
+             break e default */
+			 //neste caso não utilizamos o break e agora podemos pegar mais informações de cada caso
+    }
+    
+  }
+}
---
+++ controle-de-fluxo/src/SistemaMedia.java
@@ -0,0 +1,29 @@
+public class SistemaMedia {
+
+    public static void main(String[] args) {
+        
+        String sigla = "M";
+
+		switch (sigla) {
+		case "P":{
+			System.out.println("PEQUENO");
+			break;
+		}
+		case "M":{
+			System.out.println("MEDIO");
+			break;
+		}
+		case "G":{
+			System.out.println("GRANDE");
+			break;
+		}
+		default:
+			System.out.println("INDEFINIDO");
+		}
+			
+        /*Observe que a nível de sintaxe não tivemos nenhum ganho quanto a redução de códigos e ainda
+         tivemos mais uma preocupação em informar a palavra break em cada alternativa */
+         //No caso talvez fosse mais facil utilizar if/else
+		
+	}
+}
